b42e19296176e4cf28175d3ba973ba970079ea1d
Steven-McCombe <smccombe93@gmail.com>
2022-12-29 14:13:03 -0500

    added product columns in model

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,6 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
+const { all } = require('sequelize/types/lib/operators');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -9,9 +10,40 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
-  },
-  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+
+    },
+    price: {
+      type: DataTypes.DECIMAL(11, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
+    },
+    stock:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isDecimal: false,
+        isNumeric: true,
+      }
+    },
+    category_id:{
+      type: DataTypes.INTEGER,
+      references:{
+        model: "category",
+        key: "id"
+    }
+  }
+}, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
