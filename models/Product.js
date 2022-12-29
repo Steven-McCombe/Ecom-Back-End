7b2358d67b1a9c67a4a61e7f9327bf1b2549105c
Steven-McCombe <smccombe93@gmail.com>
2022-12-29 14:40:00 -0500

    updated models

--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,6 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-const { all } = require('sequelize/types/lib/operators');
+// const { all } = require('sequelize/types/lib/operators');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -10,17 +10,20 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    //Unique identifier
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
+    //Product name column
     product_name: {
       type: DataTypes.STRING,
       allowNull: false,
 
     },
+    //Product price column as a decimal number
     price: {
       type: DataTypes.DECIMAL(11, 2),
       allowNull: false,
@@ -28,14 +31,15 @@ Product.init(
         isDecimal: true,
       }
     },
+    //Product stock column can only be a number 
     stock:{
       type: DataTypes.INTEGER,
       allowNull: false,
       validate: {
-        isDecimal: false,
         isNumeric: true,
       }
     },
+    //Category id links product to its respective category.
     category_id:{
       type: DataTypes.INTEGER,
       references:{
